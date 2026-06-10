45c7bdf5684e5bcd68a34a75f4131c1aed7d21da
Mirasalm <mira.salmensaari@gmail.com>
2026-06-10 17:18:12 +0300

    Improve goose def file
    Currently biggest issue is that Goose seems to only read a single config file. You can however set configuration options with env variables which take precedence over anything else, so that's probably the most sensible option for important configuration

--- build_container.sh
+++ build_container.sh
@@ -3,7 +3,7 @@
 set -e
 set -o xtrace
 
-TARGET_CONTAINER="opencode"
+TARGET_CONTAINERS=("opencode" "goose")
 
 # Parses if we are on roihu-cpu or roihu-gpu.
 # TODO: Make this portable.
@@ -11,4 +11,7 @@ NODE_TYPE=$( hostname | cut -d "-" -f 2 )
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
-apptainer build --fakeroot --fix-perms --writable-tmpfs images/${TARGET_CONTAINER}-${NODE_TYPE}.sif apptainer/${TARGET_CONTAINER}.def
+
+for CONTAINER in "$TARGET_CONTAINERS" do;
+    apptainer build --fakeroot --fix-perms --writable-tmpfs images/${TARGET_CONTAINER}-${NODE_TYPE}.sif apptainer/${TARGET_CONTAINER}.def
+done
