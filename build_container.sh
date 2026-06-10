3e363f63c6fb196c74627350ac578843426d4f73
Mirasalm <mira.salmensaari@gmail.com>
2026-06-10 13:21:08 +0300

    Update build_containers and opencode.def
    Build the container with --writable-tmpfs. That runs the tests with writable filesystem, which fixes the write issues with opencode debug info

--- build_container.sh
+++ build_container.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -o xtrace
 
 TARGET_CONTAINER="opencode"
 
@@ -10,4 +11,4 @@ NODE_TYPE=$( hostname | cut -d "-" -f 2 )
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
-apptainer build images/${TARGET_CONTAINER}-${NODE_TYPE}.sif apptainer/${TARGET_CONTAINER}.def
+apptainer build --fakeroot --fix-perms --writable-tmpfs images/${TARGET_CONTAINER}-${NODE_TYPE}.sif apptainer/${TARGET_CONTAINER}.def
