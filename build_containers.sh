a9d3867fdacda4c8cb8fd3c2facdabdbad00aa6d
Mirasalm <mira.salmensaari@gmail.com>
2026-06-11 12:35:17 +0300

    Goose container builds but needs more testing

---
+++ build_containers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+TARGET_CONTAINERS=("opencode" "goose")
+TARGET_VERSIONS=("1.17.0" "1.37.0")
+
+# Parses if we are on roihu-cpu or roihu-gpu.
+# TODO: Make this portable.
+NODE_TYPE=$( hostname | cut -d "-" -f 2 )
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+# Loop over list length
+for (( i=0; i<${#TARGET_CONTAINERS[@]}; i++ )); do
+    CONTAINER=${TARGET_CONTAINERS[i]}
+    VERSION=${TARGET_VERSIONS[i]}
+    apptainer build --fakeroot --fix-perms --writable-tmpfs --build-arg "APP_VERSION=$VERSION" images/${CONTAINER}-${NODE_TYPE}-${VERSION}.sif apptainer/${CONTAINER}.def
+done
