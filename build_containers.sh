bf4ecbc0ef66b26d0c19e591e6779cf7ec7b0dae
Elias Anson <elias.anson@csc.fi>
2026-07-07 16:05:40 +0300

    Check node type and change base image for gpu nodes

--- build_containers.sh
+++ build_containers.sh
@@ -9,6 +9,11 @@ TARGET_VERSIONS=("1.17.0" "1.37.0")
 # Parses if we are on roihu-cpu or roihu-gpu.
 # TODO: Make this portable.
 NODE_TYPE=$( hostname | cut -d "-" -f 2 )
+if [[ $NODE_TYPE == "gpu" ]]; then
+    BASE_IMAGE="satama.csc.fi/r_installation_spack/core-gpu-g$"
+else
+    BASE_IMAGE="satama.csc.fi/r_installation_spack/core-cpu-g$"
+fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
@@ -32,6 +37,6 @@ mkdir -p images
 for (( i=0; i<${#TARGET_CONTAINERS[@]}; i++ )); do
     CONTAINER=${TARGET_CONTAINERS[i]}
     VERSION=${TARGET_VERSIONS[i]}
-    apptainer build --fakeroot --fix-perms --writable-tmpfs --force --build-arg "APP_VERSION=$VERSION" \
+    apptainer build --fakeroot --fix-perms --writable-tmpfs --force --build-arg "APP_VERSION=$VERSION" --build-arg "BASE_IMAGE=$BASE_IMAGE" \
         images/${CONTAINER}-${NODE_TYPE}-${VERSION}.sif apptainer/${CONTAINER}.def
 done
