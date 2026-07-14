2044df57c917874cace2a6a5fb3a386a3f2b8166
Mirasalm <mira.salmensaari@gmail.com>
2026-07-14 13:28:49 +0300

    remove redundant chgrp from build_containers.sh

--- build_containers.sh
+++ build_containers.sh
@@ -64,5 +64,4 @@ for (( i=0; i<${#TARGET_CONTAINERS[@]}; i++ )); do
 done
 chgrp -R project_2001659 images
 chgrp -R project_2001659 bin
-chgrp -R project_2001659 config/opencode/skills
 cd $OLD_DIR
