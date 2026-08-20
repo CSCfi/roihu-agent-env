67a9d7b35ee0c2b2eb3345aa386d1315d2f59344
Mirasalm <mira.salmensaari@gmail.com>
2026-08-20 15:24:29 +0300

    Add modules and building script for roihu-agent-env

---
+++ build_roihu_agent_env.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+set -euo pipefail
+
+agent_env_version=$( date +'%y.%m.%d' )
+container="roihu-agent-env"
+
+#
+# Versions to install. Change this as necessary.
+# Order is Opencode version, Claude version
+#
+target_versions=("latest", "stable")
+
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+old_dir=$(pwd)
+cd $script_dir
+
+
+create_agent_env_module () {
+    # Create a module file from template.
+    #
+    # Currently only designed for roihu-agent-env, and only replaces 
+    # __AGENT_IMAGE_NAME__ with the actual name.
+    #
+    # Usage:
+    # create_agent_env_module IMAGE_NAME IMAGE_VERSION
+    #
+
+    image_name="$1"
+    image_version="$2"
+
+    arch=$( arch )
+    if [[ $arch == "aarch64" ]]; then
+        node_type="gpu"
+    elif [[ $arch == "x86_64" ]]; then
+        node_type="cpu"
+    else
+        echo "Parsing of the processor architecture failed"
+        exit 1
+    fi
+    mkdir -p modulefiles/roihu/${node_type}/roihu-agent-env
+    sed "s/__AGENT_IMAGE_NAME__/${image_name}/" modulefiles/roihu/${node_type}/module_template \
+        > modulefiles/roihu/${node_type}/roihu-agent-env/${image_version}.lua
+}
+
+
+# Parses if we are on roihu-cpu or roihu-gpu.
+node_arch=$( arch )
+if [[ $node_arch == "aarch64" ]]; then
+    base_image="satama.csc.fi/r_installation_spack/core-gpu-gcc-14.3.0-cuda-12.9.1@sha256:96f99061fb4d21360dc89c5d1269397f85a6ad86f09479f08e07ed27b7c98311"
+    socket_bridge_file="socket-bridge-gpu"
+elif [[ $node_arch == "x86_64" ]]; then
+    base_image="satama.csc.fi/r_installation_spack/core-cpu-gcc-15.2.0@sha256:e64b470bce6bd9786d4c4f195bdb0f7827bb441c6075d0824fd5842a3aca6fe5"
+    socket_bridge_file="socket-bridge"
+else
+    echo "Parsing of the processor architecture failed"
+    exit 1
+fi
+
+
+# Build socket bridge binary
+# Only needs to be done once.
+# cd socket-bridge
+# CGO_ENABLED=0 go build -o socket-bridge .
+# cd ..
+
+
+mkdir -p images
+
+
+# Update the job-monitoring-sdk:
+rm -rf job-monitoring-sdk
+git clone --depth 1 -b main ssh://git@gitlab.ci.csc.fi:10022/compen/job-monitoring/job-monitoring-sdk.git || { echo "git clone failed. Make sure you are on roihu-install node."; exit 1; }
+
+
+# Build the container
+apptainer build --fakeroot --fix-perms --writable-tmpfs --force \
+    --build-arg "OPENCODE_VERSION=${version[1]}" \
+    --build-arg "CLAUDE_VERSION=${version[2]}" \
+    --build-arg "BASE_IMAGE=$base_image" \
+    --build-arg "SOCKET_BRIDGE_FILE=$socket_bridge_file" \
+    images/${container}-${node_arch}-${version}.sif apptainer/${container}.def
+
+# Generate modulefile
+if [[ $container == "roihu-agent-env" ]]; then
+    create_agent_env_module ${container}-${node_arch}-${version}.sif $version
+fi
+
+
+chgrp -R project_2001659 images
+chgrp -R project_2001659 bin
+cd $old_dir
